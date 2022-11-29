ca41604afe4ad090937179ea912d184735b2ac7d
Matias Sivadon <sivadon.m@gmail.com>
2022-11-29 15:40:46 -0300

    Definiendo las rutas

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,26 +1,54 @@
 import React, {useEffect, useState} from 'react';
 import Item from './Item'
-import {useParams} from 'react-router-dom';
-import {getFirestore, collection, getDocs} from 'firebase/firestore';
+import NavBar from './NavBar'
+import {useOutlet, useParams} from 'react-router-dom';
+import {getFirestore, collection, getDocs, query, where} from 'firebase/firestore';
 import '../styles/Item.css';
 
 const ItemListContainer = () => {
     const [allProducts, setAllProducts] = useState(null)
+    const [allCategories, setAllCategories] = useState(null)
+    const [isFiltering, setIsFiltering] = useState(false)
+    const [filteredProducts, setFilteredProducts] = useState(null)
+    
 
     useEffect(() => {
         const db = getFirestore()
         const itemsCollection = collection(db, 'products')
+        // const categoryCollection = collection(db, 'categories')
             getDocs(itemsCollection).then((snapshot) => {
                 const products = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data() }))
                 setAllProducts(products)
             })
-
     }, [])
-    
+
+    const onFilter =(categoryId) => {
+        const db = getFirestore()
+        const q = query(collection(db, 'products'), where('categoryId', '==', categoryId))
+
+        getDocs(q).then((snapshot) => {
+            const filterproducts = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data() }))
+            setFilteredProducts(filterproducts)
+            setIsFiltering(true)
+        })
+    }    
+
     return (
         <>
+        <nav>
+
+            {isFiltering && filteredProducts && filteredProducts.map((category) => {
+                <NavBar key={category.index} onFilter={onFilter}/>
+            })}
+
+         </nav>
+
          <h1 className='title'>BIKE PRO</h1>
+
          <div className='card-container'>
+            {isFiltering && filteredProducts && filteredProducts.map((product) => (
+                <Item key={product.id} product={product}/>
+            ))} :
             {allProducts && allProducts.map((product) => (
                 <Item key={product.id} product={product}/>
             ))}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,57 @@
+// import React, {useState, useEffect} from 'react';
+// import {useParams} from 'react-router-dom';
+// import {getFirestore, collection, getDocs, query, where} from 'firebase/firestore';
+// import CartWidget from './CartWidget';
+// import {NavLink} from 'react-router-dom';
+// import '../styles/NavBar.css';
+
+
+// const NavBar = () => {
+
+//     const [goToMtb, setGoToMtb] = useState(null)
+//     const [goToRuta, setGoToRuta] = useState(null)
+//     const {category} = useParams()
+
+//     useEffect(()=> {
+//         const db = getFirestore()
+//         const queryMtb = query(collection(db, 'categories'), where('category', '==', 'Mountain Bike'))
+//         const queryRuta = query(collection(db, 'categories'), where('category', '==', 'Ruta'))
+
+//             Promise.all ([
+//                 getDocs(queryMtb).then((snapshot) => {
+//                     setGoToMtb(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))) 
+//                 }),
+                
+//                 getDocs(queryRuta).then((snapshot) => {
+//                     setGoToRuta((snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))))
+//                 }) 
+//             ])
+//             console.log(goToMtb,goToRuta)
+//     },[category])
+
+
+//     return ( 
+//             <nav className='navbar-container'>
+//                 <ul className='navbar'>
+//                     <li className='navbar-items'>
+//                         <NavLink to='/'>HOME</NavLink>
+//                     </li>
+//                         <li className='navbar-items'>
+//                             <NavLink to={`category/${goToMtb}`}>MONTAÑA</NavLink>
+//                         </li>
+//                         <li className='navbar-items'>
+//                             <NavLink to={`category/${goToRuta}`}>RUTA</NavLink>
+//                         </li>
+//                         <li className='navbar-items-cart'>
+//                             <NavLink to='/cart'> <CartWidget/> </NavLink>
+//                         </li>
+//                 </ul>
+//             </nav> 
+//     )
+// }
+
+// export default NavBar
+
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import {getFirestore, collection, getDocs, query, where} from 'firebase/firestore';
@@ -6,29 +60,15 @@ import {NavLink} from 'react-router-dom';
 import '../styles/NavBar.css';
 
 
-const NavBar = () => {
+const NavBar = ({onFilter}) => {
 
-    const [goToMtb, setGoToMtb] = useState(null)
-    const [goToRuta, setGoToRuta] = useState(null)
-    const {category} = useParams()
+    const [urls, setUrls] = useState([])
 
     useEffect(()=> {
         const db = getFirestore()
-        const queryMtb = query(collection(db, 'categories'), where('category', '==', 'Mountain Bike'))
-        const queryRuta = query(collection(db, 'categories'), where('category', '==', 'Ruta'))
-
-            Promise.all ([
-                getDocs(queryMtb).then((snapshot) => {
-                    setGoToMtb(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))) 
-                }),
-                
-                getDocs(queryRuta).then((snapshot) => {
-                    setGoToRuta((snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))))
-                }) 
-            ])
-            console.log(goToMtb,goToRuta)
-    },[category])
-
+        const categories = collection(db, 'categories')
+        getDocs(categories).then((snapshot) =>  setUrls(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))) )
+    },[])
 
     return ( 
             <nav className='navbar-container'>
@@ -36,12 +76,13 @@ const NavBar = () => {
                     <li className='navbar-items'>
                         <NavLink to='/'>HOME</NavLink>
                     </li>
-                        <li className='navbar-items'>
-                            <NavLink to={`category/${goToMtb}`}>MONTAÑA</NavLink>
-                        </li>
-                        <li className='navbar-items'>
-                            <NavLink to={`category/${goToRuta}`}>RUTA</NavLink>
-                        </li>
+                        {urls.length && urls.map((url, index)=>{
+                            return(
+                                <li className='navbar-items' key={index} onClick={onFilter}>
+                                    <NavLink to={`category/${url.category}`}>{url.category}</NavLink>
+                                </li>
+                            )
+                        })}
                         <li className='navbar-items-cart'>
                             <NavLink to='/cart'> <CartWidget/> </NavLink>
                         </li>
